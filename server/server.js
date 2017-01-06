2fd278d159ef4e9499518ad6aeb7cf7b227889b8
sean-oneal <smoneal@gmail.com>
2017-01-05 19:15:24 -0800

    Set up Server Logs for Debugging and Connected Server to Socket.io

--- server/server.js
+++ server/server.js
@@ -1,22 +1,40 @@
 
+//  TODO: Uncomment when SSL set up
+// const https = require('https');
+const path = require('path');
+
 const express = require('express');
+const morgan = require('morgan');
+const logger = require('./utils/logger.js');
+
+
+const router = require('./routes.js');
+
 const app = express();
-const db = require('./db/db.js');
 
-// const path = require('path');
-const router = require('./routes.js')
+app.use(morgan('dev'));
+app.use('/api', router);
 
-//  TODO: Link Front-End Static Files
-// app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use('/api', router)
+app.use('./utils/logger.js', express.static(path.join(__dirname, 'allLogs.log')));
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 3008;
+//  TODO: Set up SSL connection
+//  const sslPort = 3011;
+server.listen(port, () => logger.info(`Server listening on ${port}!`));
 
 app.get('/', (req, res) => {
-  res.send('Hello from the Server!')
-})
+ res.status(200).send('Hello from Pharos server!');
+});
 
-app.listen(3000, () => {
-//  TODO: Delete testing listen function below, uncomment app.listen
-  console.log('Example app listening on port 3000!')
-})
+io.on('connection', (socket) => {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', (data) => {
+    console.log(data);
+  });
+});
 
-// app.listen(3000);
+
+module.exports = app;
