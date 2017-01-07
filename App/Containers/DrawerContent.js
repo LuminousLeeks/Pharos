c21e7251e36e1b5250552387a789c51b107698a4
michaelbdai <michaelbdai@gmail.com>
2017-01-06 16:36:08 -0800

    changed folder name

--- App/Containers/DrawerContent.js
+++ App/Containers/DrawerContent.js
@@ -1,9 +1,6 @@
-// @flow
-
 import React, { Component } from 'react'
-import { ScrollView, Image, BackAndroid } from 'react-native'
+import { ScrollView, BackAndroid } from 'react-native'
 import styles from './Styles/DrawerContentStyle'
-import { Images } from '../Themes'
 import DrawerButton from '../Components/DrawerButton'
 import { Actions as NavigationActions } from 'react-native-router-flux'
 
@@ -14,54 +11,29 @@ class DrawerContent extends Component {
       if (this.context.drawer.props.open) {
         this.toggleDrawer()
         return true
-      }
+      };
       return false
     })
-  }
+  };
 
   toggleDrawer () {
     this.context.drawer.toggle()
-  }
+  };
 
-  handlePressComponents = () => {
+  handlePressMapView = () => {
     this.toggleDrawer()
-    NavigationActions.componentExamples()
-  }
-
-  handlePressUsage = () => {
-    this.toggleDrawer()
-    NavigationActions.usageExamples()
-  }
-
-  handlePressAPI = () => {
-    this.toggleDrawer()
-    NavigationActions.apiTesting()
-  }
-
-  handlePressTheme = () => {
-    this.toggleDrawer()
-    NavigationActions.theme()
-  }
-
-  handlePressDevice = () => {
-    this.toggleDrawer()
-    NavigationActions.deviceInfo()
+    NavigationActions.mapviewExample()
   }
 
   render () {
     return (
       <ScrollView style={styles.container}>
-        <Image source={Images.logo} style={styles.logo} />
-        <DrawerButton text='Component Examples' onPress={this.handlePressComponents} />
-        <DrawerButton text='Usage Examples' onPress={this.handlePressUsage} />
-        <DrawerButton text='API Testing' onPress={this.handlePressAPI} />
-        <DrawerButton text='Themes' onPress={this.handlePressTheme} />
-        <DrawerButton text='Device Info' onPress={this.handlePressDevice} />
+        <DrawerButton text='Map View' onPress={this.handlePressMapView} />
       </ScrollView>
     )
-  }
+  };
 
-}
+};
 
 DrawerContent.contextTypes = {
   drawer: React.PropTypes.object
--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react'
 import { View, StatusBar } from 'react-native'
 import NavigationRouter from '../Navigation/NavigationRouter'
 import { connect } from 'react-redux'
-import StartupActions from '../Redux/StartupRedux'
-import ReduxPersist from '../Config/ReduxPersist'
+// import StartupActions from '../Redux/StartupRedux'
+// import ReduxPersist from '../Config/ReduxPersist'
 
 // Styles
 import styles from './Styles/RootContainerStyle'
@@ -13,10 +13,10 @@ import styles from './Styles/RootContainerStyle'
 class RootContainer extends Component {
   componentDidMount () {
     // if redux persist is not active fire startup action
-    if (!ReduxPersist.active) {
-      this.props.startup()
-    }
-  }
+    // if (!ReduxPersist.active) {
+    //   this.props.startup()
+    // };
+  };
 
   render () {
     return (
@@ -25,12 +25,12 @@ class RootContainer extends Component {
         <NavigationRouter />
       </View>
     )
-  }
-}
+  };
+};
 
 // wraps dispatch to create nicer functions to call within our component
 const mapDispatchToProps = (dispatch) => ({
-  startup: () => dispatch(StartupActions.startup())
+  // startup: () => dispatch(StartupActions.startup())
 })
 
 export default connect(null, mapDispatchToProps)(RootContainer)
