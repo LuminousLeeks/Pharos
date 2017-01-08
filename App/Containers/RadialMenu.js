94cb52cbad4d595267cd9766e33953811f620636
michaelbdai <michaelbdai@gmail.com>
2017-01-07 18:16:11 -0800

    set up the redux action file

--- App/Containers/RadialMenu.js
+++ App/Containers/RadialMenu.js
@@ -2,26 +2,32 @@ import React from 'react'
 import { Badge, Button } from 'native-base';
 
 import { View, Alert } from 'react-native'
-
-import RadialMenu from './RadialMenu_npm'
+import { connect } from 'react-redux'
+import RadialMenu from '../Components/RadialMenu_npm'
 import Styles from './Styles/RadialMenuStyles'
 import EventCategories from '../Lib/EventCategories'
-// const Icon = EventCategories.waitTime.icon
+import { reportEvent } from '../Actions'
+import { Actions as NavigationActions } from 'react-native-router-flux'
 
-// import Icon.FontAwesome from 'react-native-vector-icons/FontAwesome'
-// import FontAwesome from 'react-native-vector-icons/FontAwesome'
-
-// import IconButton from './IconButton'
 const EventCategory = EventCategories.waitTime;
 const Application = React.createClass({
-  handleSelect: function(events, description) {
+
+  handleSelect: function(events, description, key) {
     Alert.alert(
       'Report event category: ' + description,
       null,
       events.map((event) => {
         return {
           text: event,
-          onPress: (event) => {console.log(event)}
+          onPress: () => {
+            let event = {
+              category: key,
+              event: event
+            }
+            // dispatch(reportEvent(event));
+            // NavigationActions.reportEventScreen();
+
+          }
         }
       })
     )
@@ -37,7 +43,7 @@ const Application = React.createClass({
           spreadAngle={90}
           startAngle={0}
         >
-          <Badge style={Styles.root}>Report</Badge>
+          <Badge style={Styles.root}>{this.props.socket}</Badge>
           { Object.keys(EventCategories)
             .map((EventCategoryKey) => EventCategories[EventCategoryKey])
             .map((EventCategory, index) => 
@@ -46,7 +52,7 @@ const Application = React.createClass({
                 warning
                 key={index} 
                 onSelect={() => {
-                  this.handleSelect(EventCategory.events, EventCategory.description)
+                  this.handleSelect(EventCategory.events, EventCategory.description, EventCategory.key)
                 }}
               >
                 <EventCategory.icon size={30} />
@@ -58,7 +64,5 @@ const Application = React.createClass({
     )
   }
 })
-
+Application = connect()(Application);
 export default Application
-
-
--- App/Redux/index.js
+++ App/Redux/index.js
@@ -1,19 +1,50 @@
 import { combineReducers } from 'redux'
 
-const mapAction = (state = {
-  isFetching: false,
-  message: ''
-}, action) => {
+let initalState = {
+  fething: false,
+  newEvent: {},
+  userInterests: {},
+  userLocation: {},
+  userName: '',
+  toke: {},
+  events:[]
+
+}
+
+const mapAction = (state = initalState, action) => {
   switch (action.type) {
-  case 'A':
-    return Object.assign({}, state, {
-      isFetching: true
-    });
-  case 'B':
-    return Object.assign({}, state, {
-      isFetching: false,
-      message: action.data.message
-    });
+  case 'REPORT_EVENT':
+    return {
+      ...state,
+      newEvent: action.newEvent
+    };
+  case 'LOAD_EVENTS':
+    return {
+      ...state,
+      // action.events
+    };
+  case 'UPDATE_EVENTS':
+    return {
+      ...state,
+      // action.events
+    };
+  case 'GET_USER_INFO':
+    return {
+      ...state,
+      // action.userName,
+      // action.userInterests
+      // action.token
+    };
+  case 'START_FETCHING':
+    return {
+      ...state,
+      fetching: true
+    };
+  case 'STOP_FETCHING':
+    return {
+      ...state,
+      fetching: false      
+    }
   default:
     return state
   }
