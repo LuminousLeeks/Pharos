6937978466713246a7c4c067e1b0183166880af6
michaelbdai <michaelbdai@gmail.com>
2017-01-19 17:40:53 -0800

    Revert "Feat/show vote count"

--- initArchieve.sql
+++
@@ -1,9 +0,0 @@
-/* Set up the database*/
-
-/* Enable postgis*/
-
-CREATE DATABASE pharos_archieve OWNER pharos_admin;
-\c pharos_archieve pharos_admin
-CREATE EXTENSION postgis;
-\q
-/* Environment Data*/
