c0a65d7a30f2cffc2138fda4efd0494c88f7d5eb
sean-oneal <smoneal@gmail.com>
2017-02-18 17:11:54 -0800

    (fix/cleanup)Fix username variable, refactor code in lint style

--- App/Containers/NotificationScreen.js
+++ App/Containers/NotificationScreen.js
@@ -1,37 +1,37 @@
-import React, { PropTypes } from 'react'
-import { connect } from 'react-redux'
-import Notification from '../Components/Notification'
-import { sendVoteToState, sendVoteToServer } from '../Actions'
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import Notification from '../Components/Notification';
+import { sendVoteToState, sendVoteToServer } from '../Actions';
 
 const mapStateToProps = (state, ownProps) => ({
   event: ownProps.event,
 
-  username: state.username
-})
+  username: state.username,
+});
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   handleThumbsUpIsPressed: () => {
     dispatch(sendVoteToServer({
-      userName: ownProps.userName,
+      username: ownProps.username,
       eventId: ownProps.event.eventId,
-      vote: true
-    }))
+      vote: true,
+    }));
   },
   handleThumbsDownIsPressed: () => {
     dispatch(sendVoteToServer({
-      userName: ownProps.userName,
+      username: ownProps.username,
       eventId: ownProps.event.eventId,
-      vote: false
-    }))
-  }
-})
+      vote: false,
+    }));
+  },
+});
 
-const NotificationScreen = connect(mapStateToProps, mapDispatchToProps)(Notification)
+const NotificationScreen = connect(mapStateToProps, mapDispatchToProps)(Notification);
 
 NotificationScreen.propTypes = {
   event: PropTypes.object,
-  userName: PropTypes.string,
-  events: PropTypes.object
-}
+  username: PropTypes.string,
+  events: PropTypes.object,
+};
 
-export default NotificationScreen
+export default NotificationScreen;
